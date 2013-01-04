fda30a20f89775ef3a089a6133186fc2b4a6ef11
pupunzi <matteo@open-lab.com>
2013-01-04 17:35:16 +0100

    various

--- inc/extract.js
+++ inc/extract.js
@@ -2,27 +2,22 @@
 /*
  * ******************************************************************************
  *  jquery.mb.components
+ *  file: extract.js
  *
- *  Copyright (c) 2001-2013. Matteo Bicocchi (Pupunzi); Open lab srl, Firenze - Italy
+ *  Copyright (c) 2001-2013. Matteo Bicocchi (Pupunzi);
+ *  Open lab srl, Firenze - Italy
  *  email: matteo@open-lab.com
- *  site: http://pupunzi.com
+ *  site: 	http://pupunzi.com
+ *  blog:	http://pupunzi.open-lab.com
+ * 	http://open-lab.com
  *
  *  Licences: MIT, GPL
  *  http://www.opensource.org/licenses/mit-license.php
  *  http://www.gnu.org/licenses/gpl.html
+ *
+ *  last modified: 04/01/13 17.13
  *  *****************************************************************************
  */
-
-/*******************************************************************************
- jquery.mb.components
- Copyright (c) 2001-2011. Matteo Bicocchi (Pupunzi); Open lab srl, Firenze - Italy
- email: mbicocchi@open-lab.com
- site: http://pupunzi.com
-
- Licences: MIT, GPL
- http://www.opensource.org/licenses/mit-license.php
- http://www.gnu.org/licenses/gpl.html
- ******************************************************************************/
 
 buildBookletPage=function(el){
   var page= new Object();
